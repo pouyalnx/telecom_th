3ce6f8b6fc68e1293f5e4109d23d120091a78a48
pouyalnx <pouyanbie@gmail.com>
2021-06-08 03:37:22 -0400

    i m so happy at end learn how make ayanc with thread searcher will be find

--- telclinet.py
+++ telclinet.py
@@ -39,19 +39,19 @@ def chat2entity(usr): #id title
 key_contacts="contacts"
 key_chats_id="chat_id"
 key_new_message="new_message"
+key_new_message_id="new_message_id"
 
 stack[key_contacts]=[]
 stack[key_chats_id]=[]
 stack[key_new_message]=[]
+stack[key_new_message_id]=[]
 
 ####################################################################
 #events
 @client.on(NewMessage(incoming=True))
 async def newMessage(event:NewMessage.Event):
     stack[key_new_message].append(event.chat_id)
-    print("*icnome_message*")
-    print(type(event.chat))
-    print(type(event.message))
+    print("**icnome_message**")
 
 
 
@@ -80,7 +80,7 @@ async def ainput():
 ###############################################################################################################################
 async def handler(client):
     while True:
-        print("enter command\n\texit\n\tcontacts <pattern>\n\tsend <index> <text|file> <msg|filename>\n\tchat <pattern>")
+        print("enter command\n\texit\n\tcontacts <pattern>\n\tsend <index> <text|file> <msg|filename>\n\tchat <pattern>\n\tinbox [clr]")
         data=(await ainput()).split(" ")
         cmd=data.pop(0)
         ###########################################################################################################################
@@ -172,9 +172,34 @@ async def handler(client):
                     txt+=f"{info} "
                 print(f"|\tc{i}\t|\t{txt}")    
             print(f"results {len(stack[key_chats_id])}")
+        ##############################################################################################################################
+        elif cmd=="inbox":
+            if data==[]:
+                if len(stack[key_new_message]):
+                    checked=[]
+                    stack[key_new_message_id]=[]
+                    ptr=0
+                    for chat_id in reversed(stack[key_new_message]):
+                        if not(chat_id in checked):
+                            checked.append(chat_id)
+                            cnt=stack[key_new_message].count(chat_id)
+                            async for msg in client.iter_messages(chat_id,from_user=chat_id,):
+                                if cnt==0:
+                                    break
+                                cnt-=1
+                                stack[key_new_message_id].append([chat_id])
+                                print(f"i{ptr}\t{msg.name}\t{msg.raw_text} {}")
+                                ptr+=1
+                else:
+                    print("stack is empty")
+            elif data[0]=="clr":
+                stack[key_new_message]=[] #inbox has been cleared
+                print("stack cleared")
+            else:
+                print("I can't decode ur input")
+
 
 #####################################################################
-
 client.start()
 client.loop.create_task(handler(client))
 print("telegram connected...")
